cbc33001f6be3a3e572468c541b19d55b966c676
ijalrusyadi <rizalimran@gmail.com>
2018-10-21 19:36:44 +0700

    add

---
+++ operator_aritmatika.py
@@ -0,0 +1,11 @@
+
+# file: operator_aritmatika.py
+
+# Ambil input untuk mengisi nilai
+a = input("Inputkan nilai a: ")
+b = input("Inputkan nilai b: ")
+
+# Menggunakan operator penjumlahan
+c = a + b
+print ("Hasil %r + %r = %r" % (a,b,c))
+
